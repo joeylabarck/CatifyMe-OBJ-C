e06066bc610551456496ab184cc565a11ee9ae73
Joey LaBarck <joeylabarck@gmail.com>
2015-01-22 11:57:48 -0500

    Initial Commit

---
+++ CatifyMe-OBJ-C/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CatifyMe-OBJ-C
+//
+//  Created by Joey LaBarck on 1/22/15.
+//  Copyright (c) 2015 LaBarck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ CatifyMe-OBJ-CTests/CatifyMe_OBJ_CTests.m
@@ -0,0 +1,40 @@
+//
+//  CatifyMe_OBJ_CTests.m
+//  CatifyMe-OBJ-CTests
+//
+//  Created by Joey LaBarck on 1/22/15.
+//  Copyright (c) 2015 LaBarck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface CatifyMe_OBJ_CTests : XCTestCase
+
+@end
+
+@implementation CatifyMe_OBJ_CTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
